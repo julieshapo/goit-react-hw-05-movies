2a766f84656293e45f9a87e62c844d70b24c57dc
Julie Shapovalenko <jools.shapo@gmail.com>
2023-05-13 01:03:51 +0300

    delete basename

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/julieshapo">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
